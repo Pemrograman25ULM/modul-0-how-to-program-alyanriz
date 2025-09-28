f36c18220a026856e075995e2a0f947640f77edd
Nayla Rizkia <naylarizkvia@gmail.com>
2025-09-28 21:36:19 +0800

    Add files via upload

---
+++ C/PRAK0002-2510817120007-NAYLA_RIZKIA.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    char Nama[] = "Nayla Rizkia";
+    printf("Selamat Pagi, %s\n", Nama);
+    printf("Selamat Siang, %s\n", Nama);
+    printf("Selamat Malam, %s\n", Nama);
+
+    return 0;
+}
---
+++ C/PRAK0004-2510817120007-NAYLA_RIZKIA.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    printf("##############################\n");
+    printf("#			     #\n");
+    printf("#	Nayla Rizkia	     #\n");
+    printf("#	2510817120007	     #\n");
+    printf("#			     #\n");
+    printf("##############################\n");
+
+    return 0;
+}
